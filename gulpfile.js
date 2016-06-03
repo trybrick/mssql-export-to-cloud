0e7c5218a85d92b18c1f1e30ccffe5fbe2f66fee
noogen <tom@noogen.net>
2016-06-03 10:53:02 -0500

    separate by type

--- gulpfile.js
+++ gulpfile.js
@@ -164,7 +164,7 @@ gulp.task('upload', function() {
       manualContentEncoding: 'gzip',
       keyTransform: function(relative_filename) {
         // add yy mm dd to filename
-        var new_name = 'exports/' + today.format("YYYYMMDD") + '/' + relative_filename;
+        var new_name = 'exports/' + etype + '/' + relative_filename;
         console.log(new_name);
         // or do whatever you want 
         return new_name;
