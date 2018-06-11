c175a7d196b59991786bf07b063ef651998c34bb
Tom Noogen <tom@noogen.net>
2018-06-11 17:11:17 -0500

    fix syntax

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ module.exports = {
       rowDelimiter: '\n'
     },
     productdb: {
-      query: 'SELECT [upc], ('0' + LEFT([upc], 13)) upcnc, [name], [brand], [department] as dept, [aisle], [category] as cat, [shelf], [imageurl] as img FROM [dbo].[Product2] WITH (NOLOCK) WHERE ISNUMERIC(upc) = 1 AND LEN(UPC) = 14 AND CAST(LEFT(UPC,14) AS BIGINT) > 999999',
+      query: 'SELECT [upc], (\'0\' + LEFT([upc], 13)) upcnc, [name], [brand], [department] as dept, [aisle], [category] as cat, [shelf], [imageurl] as img FROM [dbo].[Product2] WITH (NOLOCK) WHERE ISNUMERIC([upc]) = 1 AND LEN([upc]) = 14 AND CAST(LEFT([upc],14) AS BIGINT) > 999999',
       output: 'products.psv',
       compressFile: false,
       outputSingleFile: true,
